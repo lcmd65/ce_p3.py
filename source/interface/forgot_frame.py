7781162263921b7421816fd859a251c0c1c17b3f
lechonminhdat <dat.lemindast@gmail.com>
2023-06-28 22:42:59 +0700

    change

--- source/interface/forgot_frame.py
+++ source/interface/forgot_frame.py
@@ -1,4 +1,5 @@
 import tkinter
+import meta.external_var
 from tkinter import *
 
 class ForgotFrame(Frame):
@@ -9,5 +10,7 @@ class ForgotFrame(Frame):
         
     def initUI(self):
         self.parent.title = "Forgot Password"
+        
+        label = Label(self, i = meta.external_var.bg)
         
         
